6e8dd7a15669920d9ff07878e08dd240eaaded70
veekthor04 <victoradenuga04@yahoo.com>
2020-05-12 17:26:08 +0100

    heroku

--- pizza/wsgi.py
+++ pizza/wsgi.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizza.settings")
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
